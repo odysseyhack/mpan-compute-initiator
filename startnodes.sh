15497213a2e901822675131147965ccaab29f93d
Erik Weitenberg <erik.weitenberg@tno.nl>
2019-04-13 12:39:38 +0200

    direct output

--- startnodes.sh
+++ startnodes.sh
@@ -2,11 +2,11 @@
 
 # start the compute initiators (blockchain (-> CI -> mpc node) x5)
 
-/home/mpc/compute-initiator/main -filename /tmp/nodepipe1&
-/home/mpc/compute-initiator/main -filename /tmp/nodepipe2&
-/home/mpc/compute-initiator/main -filename /tmp/nodepipe3&
-/home/mpc/compute-initiator/main -filename /tmp/nodepipe4&
-/home/mpc/compute-initiator/main -filename /tmp/nodepipe5&
+/home/mpc/compute-initiator/main -filename /tmp/nodepipe1 > /tmp/compute-initiator-log1 &
+/home/mpc/compute-initiator/main -filename /tmp/nodepipe2 > /tmp/compute-initiator-log2 &
+/home/mpc/compute-initiator/main -filename /tmp/nodepipe3 > /tmp/compute-initiator-log3 &
+/home/mpc/compute-initiator/main -filename /tmp/nodepipe4 > /tmp/compute-initiator-log4 &
+/home/mpc/compute-initiator/main -filename /tmp/nodepipe5 > /tmp/compute-initiator-log5 &
 
 /home/mpc/gabriele/node.py /tmp/nodepipe1&
 /home/mpc/gabriele/node.py /tmp/nodepipe2&
