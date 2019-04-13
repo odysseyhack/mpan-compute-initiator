46c1607d6442e6984fb2670b5a7f00706c3c7365
Erik Weitenberg <erik.weitenberg@tno.nl>
2019-04-13 09:39:47 +0200

    secondary contract

--- smartcontract/smartcontract.go
+++ smartcontract/smartcontract.go
@@ -1,42 +1,51 @@
 package smartcontract
 
-//go:generate abigen -abi gate_keeper.abi -pkg smartcontract -type GateKeeper -out gatekeeper.go
+//go:generate abigen -abi gate_keeper.abi -pkg smartcontract -type ComputeContract -out gatekeeper.go
 
 import (
 	"log"
 	"math/big"
-	"os"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/event" //temp
 	"github.com/odysseyhack/mpan-compute-initiator/mpc"
 )
 
 const (
 	SMARTCONTRACT_ADDRESS = "0x368f79382cc5a7b769134369a2de7f5b97b28041"
-	ETHEREUM_URL          = "ws://localhost:8546"
+	ETHEREUM_URL          = "ws://134.221.210.18:8546"
 )
 
 func WaitForQuery(queryChan chan mpc.Query) {
-	conn, err := ethclient.Dial(viper.GetString("eth.url"))
+	conn, err := ethclient.Dial(ETHEREUM_URL)
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
-	// Instantiate the contract and display its name
-	gatekeeper, err := NewGateKeeper(common.HexToAddress(SMARTCONTRACT_ADDRESS), conn)
 
+	gatekeeper, err := NewGateKeeper(common.HexToAddress(SMARTCONTRACT_ADDRESS), conn)
 	if err != nil {
-		log.Fatalf("Failed to instantiate the Greeting contract: %v", err)
+		log.Fatalf("Failed to instantiate the GateKeeper contract: %v", err)
+	}
+	computeAddress, err := gatekeeper.GetContractAddress()
+	if err != nil {
+		log.Fatalf("Failed to talk to the GateKeeper contract: %v", err)
+	}
+
+	// Instantiate the contract and display its name
+	computeContract, err := NewComputeContract(computeAddress, conn)
+	if err != nil {
+		log.Fatalf("Failed to instantiate the Compute contract: %v", err)
 	}
 
 	log.Println("Waiting for smart-contract approved queries.")
 
-	var realEventChannel = make(chan *GateKeeperNewTrigger)
+	var realEventChannel = make(chan *ComputeContractNewTrigger)
 	var blockNumber uint64 = 1
 	opts := &bind.WatchOpts{}
 	opts.Start = &blockNumber
-	_, err = gatekeeper.WatchNewTrigger(opts, realEventChannel)
+	_, err = computeContract.WatchNewTrigger(opts, realEventChannel)
 	if err != nil {
 		log.Fatal("Unable to subscribe to event!", err)
 	}
@@ -44,20 +53,46 @@ func WaitForQuery(queryChan chan mpc.Query) {
 	for {
 
 		event := <-realEventChannel
-		log.Println("Received a real query request from smart contract.")
+		log.Println("(SmartContract) Received a real query request from smart contract.")
 
 		// Now create a query
-		var q mpc.Query
-		q.Compute = make([]mpc.QueryComputation, 1)
-		q.Compute[0] = mpc.QueryComputation{Function: "Info", Identifier: string(event.Identifier)}
-		q.ID = event.QueryId.String()
-		if !event.MinimalResultSize.IsUint64() {
-			log.Fatalf("Minimal result size does not fit in uin64: %v", event.MinimalResultSize.String())
+		q := mpc.Query{
+			QueryType:  mpc.QueryType(event.QueryType),
+			Identifier: int(event.Identifier),
+			Attribute:  int(event.Attribute),
+			QueryId:    event.QueryId,
 		}
-		q.MinimalResultSize = event.MinimalResultSize.Uint64()
-		log.Printf("q: %v\n", q)
+
+		log.Printf("(SmartContract) q: %v\n", q)
 
 		queryChan <- q
 
 	}
+}
+
+// temporary
+type ComputeContract struct{}
+type ComputeContractNewTrigger struct {
+	QueryType  int
+	Identifier int
+	Attribute  int
+	QueryId    *big.Int
+}
+
+func NewComputeContract(address common.Address, backend bind.ContractBackend) (*ComputeContract, error) {
+	return nil, nil
+}
+
+func (_ComputeContract *ComputeContract) WatchNewTrigger(opts *bind.WatchOpts, sink chan<- *ComputeContractNewTrigger) (event.Subscription, error) {
+	return nil, nil
+}
+
+func NewGateKeeper(address common.Address, backend bind.ContractBackend) (*GateKeeper, error) {
+	return nil, nil
+}
+
+type GateKeeper struct{}
+
+func (_GateKeeper *GateKeeper) GetContractAddress() (common.Address, error) {
+	return common.HexToAddress("0x0"), nil
 }
